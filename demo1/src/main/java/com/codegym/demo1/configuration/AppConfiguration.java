55c2062144cc07f7fa608c3cd078655ac5a405ee
Dungndph20697 <dungthoai321@gmail.com>
2025-09-17 08:00:55 +0700

    th v

--- demo1/src/main/java/com/codegym/demo1/configuration/AppConfiguration.java
+++ demo1/src/main/java/com/codegym/demo1/configuration/AppConfiguration.java
@@ -1,12 +1,16 @@
 package com.codegym.demo1.configuration;
 
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -16,8 +20,12 @@ import org.thymeleaf.templatemode.TemplateMode;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.codegym.demo1")
+@PropertySource("classpath:upload_file.properties")
 public class AppConfiguration implements WebMvcConfigurer, ApplicationContextAware {
     private ApplicationContext applicationContext;
+
+    @Value("${file-upload}")
+    private String upload;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
@@ -48,6 +56,17 @@ public class AppConfiguration implements WebMvcConfigurer, ApplicationContextAwa
         templateResolver.setTemplateMode(TemplateMode.HTML);
         templateResolver.setCharacterEncoding("UTF-8");
         return templateResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:" + upload);
+    }
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
 
 }
---
+++ demo1/src/main/java/com/codegym/demo1/th/uploadfile/controller/ProductController1.java
@@ -0,0 +1,62 @@
+package com.codegym.demo1.th.uploadfile.controller;
+
+import com.codegym.demo1.th.uploadfile.model.Product;
+import com.codegym.demo1.th.uploadfile.model.ProductFrom;
+import com.codegym.demo1.th.uploadfile.service.IProductService;
+import com.codegym.demo1.th.uploadfile.service.ProductService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("/th/updloadfile/product")
+public class ProductController1 {
+    @Value("${file-upload}")
+    private String fileUpload;
+
+    private final IProductService productService = new ProductService();
+
+    @GetMapping("")
+    public String index(Model model){
+        List<Product> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "/th/updloadfile/index";
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/th/updloadfile/create");
+        modelAndView.addObject("productForm", new ProductFrom());
+        return modelAndView;
+
+    }
+
+    @PostMapping("/save")
+    public ModelAndView saveProduct(@ModelAttribute("productForm") ProductFrom productForm) {
+        MultipartFile multipartFile = productForm.getImage();
+        String fileName = multipartFile.getOriginalFilename();
+        try {
+            FileCopyUtils.copy(productForm.getImage().getBytes(), new File(fileUpload + fileName));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        Product product = new Product(productForm.getId(), productForm.getName(),
+                productForm.getDescription(), fileName);
+        productService.save(product);
+        ModelAndView modelAndView = new ModelAndView("/th/updloadfile/create");
+        modelAndView.addObject("productForm", productForm);
+        modelAndView.addObject("message", "Created new product successfully !");
+        return modelAndView;
+    }
+}
