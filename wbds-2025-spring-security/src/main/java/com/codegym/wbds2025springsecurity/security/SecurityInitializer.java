17c84f13130f3921a47cf9d867bacb11cb71bb4c
Dungndph20697 <dungthoai321@gmail.com>
2025-10-08 09:11:50 +0700

    th Sử dụng Spring Security

---
+++ wbds-2025-spring-security/src/main/java/com/codegym/wbds2025springsecurity/security/SecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.codegym.wbds2025springsecurity.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
