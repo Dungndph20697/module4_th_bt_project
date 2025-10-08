c4a9eec8572b0da086c7ee1eb2f92a40628fce0c
Dungndph20697 <dungthoai321@gmail.com>
2025-10-08 09:35:43 +0700

    th Phân quyền dựa trên vai trò

---
+++ wbds-2025-spring-security-authorization/src/main/java/com/codegym/wbds2025springsecurityauthorization/security/SecurityInitalizer.java
@@ -0,0 +1,6 @@
+package com.codegym.wbds2025springsecurityauthorization.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInitalizer extends AbstractSecurityWebApplicationInitializer {
+}
