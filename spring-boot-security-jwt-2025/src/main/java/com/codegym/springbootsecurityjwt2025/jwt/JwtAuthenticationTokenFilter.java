03d431279afa79b7f19a18e456c94287505f79c3
Dungndph20697 <dungthoai321@gmail.com>
2025-10-13 10:50:32 +0700

    th Spring Boot JSON Web Token

--- spring-boot-security-jwt-2025/src/main/java/com/codegym/springbootsecurityjwt2025/jwt/JwtAuthenticationTokenFilter.java
+++ spring-boot-security-jwt-2025/src/main/java/com/codegym/springbootsecurityjwt2025/jwt/JwtAuthenticationTokenFilter.java
@@ -1,10 +1,16 @@
 package com.codegym.springbootsecurityjwt2025.jwt;
 
+import com.codegym.springbootsecurityjwt2025.jwt.service.JwtService;
+import com.codegym.springbootsecurityjwt2025.service.UserService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
